7ec2296bf0cc76fb2a92e0b3a4f102a1ca516404
The Exorcist <whitebeard086@gmail.com>
2022-09-21 15:57:42 +0100

    Authenticated State

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,6 +27,8 @@ class RegisterController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        auth()->attempt($request->only('email', 'password'));
+
         return redirect()->route('dashboard');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,18 +22,22 @@
         </ul>
 
         <ul class="flex items-center">
-            <li>
-                <a href="/" class="p-3">Alex</a>
-            </li>
-            <li>
-                <a href="/" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}" class="p-3">Register</a>
-            </li>
-            <li>
-                <a href="/" class="p-3">Logout</a>
-            </li>
+            @auth
+                <li>
+                    <a href="/" class="p-3">Alex</a>
+                </li>
+                <li>
+                    <a href="/" class="p-3">Logout</a>
+                </li>
+            @endauth
+            @guest
+                <li>
+                    <a href="/" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li
+            @endguest 
         </ul>
     </nav>
     @yield('content')
