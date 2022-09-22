233241b6a831e7e1c2a239fd441a9fceb5111f27
The Exorcist <whitebeard086@gmail.com>
2022-09-22 10:32:39 +0100

    Listing Posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-md">
-            <form action="{{ route('posts') }}" method="post">
+            <form action="{{ route('posts') }}" method="post" class="mb-4">
                 @csrf
                 <div class="mb-4">
                     <label for="body" class="sr-only">Body</label>
@@ -20,6 +20,18 @@
                     <button type="submit" class="bg-laravel hover:bg-black text-white px-4 py-2 rounded font-medium">Post</button>
                 </div>
             </form>
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text sm">{{ $post->created_at->diffForHumans() }}</span>
+
+                        <p class="mb-2">{{ $post->body }}</p>
+                    </div>
+                @endforeach
+            @else
+                <p>It's lonely here, post something!</p>
+            @endif
         </div>
     </div>
 @endsection
