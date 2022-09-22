bb2f45932989dcfdb076cbe673c21f6abd6530c4
The Exorcist <whitebeard086@gmail.com>
2022-09-22 09:47:28 +0100

    Ssetting Up Posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,8 +2,24 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12 bg-black text-white p-6 rounded-md">
-            Posts
+        <div class="w-8/12 bg-white text-white p-6 rounded-md">
+            <form action="{{ route('posts') }}" method="post">
+                @csrf
+                <div class="mb-4">
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something!"></textarea>
+
+                    @error('body')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div>
+                    <button type="submit" class="bg-laravel hover:bg-black text-white px-4 py-2 rounded font-medium">Post</button>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
