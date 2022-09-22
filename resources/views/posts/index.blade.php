c2cedee9711a79bb331a5ca2a7083d8ea8b14d26
The Exorcist <whitebeard086@gmail.com>
2022-09-22 10:53:15 +0100

    Pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,6 +29,8 @@
                         <p class="mb-2">{{ $post->body }}</p>
                     </div>
                 @endforeach
+
+                {{ $posts->links() }}
             @else
                 <p>It's lonely here, post something!</p>
             @endif
